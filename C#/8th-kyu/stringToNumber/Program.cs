1ed1b5eafe50a134e4da92f0bc722bdc944c68f0
Ryan Allred <ryanallred91@gmail.com>
2023-03-23 14:26:30 -0500

    completed codewar challenge

---
+++ C#/8th-kyu/stringToNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace stringToNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static int StringToNumber(string str){
+                return int.Parse(str);
+            }
+            Console.WriteLine(StringToNumber("24"));
+            Console.WriteLine("24");
+        }
+    }
+}
