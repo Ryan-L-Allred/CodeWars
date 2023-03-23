1888a2924e58b358a9409649fc34e2af6fbf8ec8
Ryan Allred <ryanallred91@gmail.com>
2023-03-23 12:43:07 -0500

    finished C# challenge

---
+++ C#/8th-kyu/ConvertBoolToString/Kata.cs
@@ -0,0 +1,25 @@
+using System;
+
+public static class Kata
+{
+  public static string boolToWord(bool word)
+  {
+    //TODO
+    // switch (word)
+    // {
+    //     case true:
+    //     return "Yes";
+    //     break;
+    //     case false:
+    //     return "No";
+    //     break;
+    // }
+    if (word == true)
+    {
+        return "Yes";
+    } else
+    {
+        return "No";
+    }
+  }
+}
---
+++ C#/8th-kyu/ConvertBoolToString/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConvertBoolToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.boolToWord(true));
+        }
+    }
+}
