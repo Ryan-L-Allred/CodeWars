23692b59ec69f57f0c5f69db59843811e65af0ee
Ryan Allred <ryanallred91@gmail.com>
2023-03-26 14:38:31 -0500

    area or perim challenge

---
+++ C#/8th-kyu/AreaOrPerimeter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AreaOrPerimeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static int AreaOrPerimeter(int l, int w) 
+            {
+                // code goes here
+                if (l == w)
+                {
+                    return l * w;
+                } else 
+                {
+                    return (l * 2) + (w * 2);
+                }
+            }
+
+            Console.WriteLine(AreaOrPerimeter(5, 6));
+        }
+    }
+}
