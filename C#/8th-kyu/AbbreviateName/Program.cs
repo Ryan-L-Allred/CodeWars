1ac1166b593be498f05cb454e6876afd804a6560
Ryan Allred <ryanallred91@gmail.com>
2023-03-26 14:15:35 -0500

    solved codewars problem

---
+++ C#/8th-kyu/AbbreviateName/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AbbreviateName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static string AbbrevName(string name)
+          {
+            //Need to split both words at the space
+            // git 
+            string[] splitName = name.Split(' ');
+            
+            return (splitName[0][0] + "." + splitName[1][0]).ToUpper();
+            
+          }
+
+          Console.WriteLine(AbbrevName("ryan allred"));
+        }
+    }
+}
---
+++ C#/8th-kyu/CountByX/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountByX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          static string AbbrevName(string name)
+          {
+            //Need to split both words at the space
+            // 
+            //string capitalized = name.ToUpper();
+            string[] splitName = name.Split(' ');
+            
+            return (splitName[0][0] + "." + splitName[1][0]).ToUpper();
+            
+          }
+
+          Console.WriteLine(AbbrevName("ryan lee allred"));
+
+            // string name = "Ryan Allred";
+            // string[] splitName = name.Split(' ');
+            // foreach (string word in splitName)
+            // {
+            //     Console.WriteLine(word);
+            // }
+            
+        }
+    }
+}
