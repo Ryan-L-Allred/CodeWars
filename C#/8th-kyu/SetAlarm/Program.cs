91d363b68721c41474ad6861a70b3c7cadbd9f3f
Ryan Allred <ryanallred91@gmail.com>
2023-03-27 10:45:39 -0500

    Set Alarm challenge finished

---
+++ C#/8th-kyu/SetAlarm/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SetAlarm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static bool SetAlarm(bool employed, bool vacation)
+            {
+                if (employed == true && vacation == false) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+            Console.WriteLine(SetAlarm(false, false));
+        }
+    }
+}
