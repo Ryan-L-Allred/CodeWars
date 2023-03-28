ae9f53f204898a137e0e37229a081380aae0399e
Ryan Allred <ryanallred91@gmail.com>
2023-03-28 10:21:08 -0500

    Character recognition challenge

---
+++ C#/8th-kyu/CharacterRecognition/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CharacterRecognition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The character recognition software is misinterpreting text. 
+            // Certain letters are being misinterpreted as numbers.
+            // "S" is being misinterpreted as "5"
+            // "O" is being misinterpreted as "0"
+            // "I" is being misinterpreted as "1"
+            // The Correct method should correct these specific mistakes when a string is past through.
+
+            static string Correct(string text)
+            {
+                string correctedLetters = text.Replace("0", "O").Replace("5", "S").Replace("1", "I");
+                return correctedLetters;
+            }
+            
+            // Outputs "SINGAPORE"
+            Console.WriteLine(Correct("51NGAP0RE"));
+        }
+    }
+}
+            
+                
