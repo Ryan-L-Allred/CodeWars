cd297a2121dec41f586b7357417ec8846ef2105a
Ryan Allred <ryanallred91@gmail.com>
2023-03-29 14:52:02 -0500

    Multiply challenge

---
+++ C#/8th-kyu/SimpleMultiplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SimpleMultiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static int Multiply(int x)
+            {
+                if (x % 2 == 0)
+                {
+                    return x * 8;
+                } 
+                else
+                {
+                    return x * 9;
+                }
+            }
+
+            Console.WriteLine(Multiply(3));
+        }
+    }
+}
