d5d4ed6eb3f741a5f7d8eb5f357656e1a2aa04fe
Ryan Allred <ryanallred91@gmail.com>
2023-05-09 15:35:06 -0500

    finished disemvowel challenge

---
+++ C#/7th-kyu/DisemvowelTrolls/Program.cs
@@ -0,0 +1,13 @@
+﻿//Function accepts a string as an argument and should return that string with all vowels removed
+//Check if string contains particular vowels
+static string Disemvowel(string str)
+{
+    var charsToRemove = new string[] {"A", "a", "E", "e", "I", "i", "O", "o", "U", "u"};
+    foreach (var c in charsToRemove)
+    {
+        str = str.Replace(c, string.Empty);
+    }
+    return str;
+}
+
+Console.WriteLine(Disemvowel("You suck"));//Expected output: "ppl"
