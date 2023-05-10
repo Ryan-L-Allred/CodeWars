7c6085b509a77b39fece8788a4f7c1ad1a90d0ac
Ryan Allred <ryanallred91@gmail.com>
2023-05-10 16:06:34 -0500

    finished How Good Are You challenge

---
+++ C#/8th-kyu/HowGoodAreYou/Program.cs
@@ -0,0 +1,13 @@
+﻿
+static bool BetterThanAverage(int[] ClassPoints, int YourPoints)
+{
+    int sum = ClassPoints.Sum();
+    int arrayLength = ClassPoints.Length;
+    return sum / arrayLength < YourPoints ? true : false;
+    
+}
+
+int[] array = new int[] {50, 50, 45, 60, 70};
+
+Console.WriteLine(BetterThanAverage(array, 85)); //expected output: True
+Console.WriteLine(BetterThanAverage(array, 30)); //expected output: False
