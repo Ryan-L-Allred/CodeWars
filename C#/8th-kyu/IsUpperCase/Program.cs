133912be1922602576995b1dec20a22cdceb734e
Ryan Allred <ryanallred91@gmail.com>
2023-05-11 14:53:03 -0500

    finished check if upper case challenge

---
+++ C#/8th-kyu/IsUpperCase/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+static bool IsUpperCase(string text)
+{
+    string capitalString = text.ToUpper();
+    return text.Contains(capitalString) ? true : false;
+}
+
+Console.WriteLine(IsUpperCase("RYAn"));
