d12158b306a84b77e214d231c668834a2d61a2e9
Ryan Allred <ryanallred91@gmail.com>
2023-07-20 10:08:32 -0500

    finished Plural problem

---
+++ C#/8th-kyu/Plural/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Plural
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           static bool Plural(double n)
+           {
+                return n != 1 ? true : false;
+           }
+
+           Console.WriteLine(Plural(0));
+        }
+    }
+}
